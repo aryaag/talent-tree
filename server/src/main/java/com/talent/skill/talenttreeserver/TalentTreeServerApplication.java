5874ead829b588bda2b0f3109a8bf3f6d23d91ae
arya-aag <aryaadarshagautam@gmail.com>
2018-05-05 13:36:55 +0530

    Initialize server-side code with Sprint Maven

---
+++ server/src/main/java/com/talent/skill/talenttreeserver/TalentTreeServerApplication.java
@@ -0,0 +1,12 @@
+package com.talent.skill.talenttreeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentTreeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentTreeServerApplication.class, args);
+	}
+}
